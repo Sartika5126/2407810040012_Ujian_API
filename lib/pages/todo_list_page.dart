2604ad6b7d0716b7cd8f613cd4f028b64166f01a
unknown <sartika.lp3i.krw24@gmail.com>
2025-12-11 21:29:34 +0700

    6

--- lib/pages/todo_list_page.dart
+++ lib/pages/todo_list_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:sartika_2407810040012_ujian_api/models/todo_model.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
 class TodoListScreen extends StatefulWidget {
   const TodoListScreen({super.key});
@@ -33,8 +35,65 @@ class _TodoListScreenState extends State<TodoListScreen> {
     }
   }
   
-  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Daftar Tugas (Todos)'),
+        backgroundColor: Colors.teal,
+        foregroundColor: Colors.white,
+        automaticallyImplyLeading: false, 
+      ),
+      body: FutureBuilder<List<TodoModel>>(
+        future: _futureTodos,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          } else if (snapshot.hasError) {
+            return Center(
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Text(
+                  'Gagal memuat data: ${snapshot.error}',
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(color: Colors.red),
+                ),
+              ),
+            );
+          } else if (snapshot.hasData) {
+
+            final List<TodoModel> todos = snapshot.data!;
+
+            return ListView.builder(
+              itemCount: todos.length,
+              itemBuilder: (context, index) {
+                final todoItem = todos[index];
+                
+                final isCompleted = todoItem.completed;
+
+                final color = isCompleted ? Colors.green.shade700 : Colors.grey.shade600;
+                final icon = isCompleted ? Icons.check_circle : Icons.radio_button_unchecked; 
+
+                return ListTile(
+                    title: Text(
+                        todoItem.todo, 
+                        style: TextStyle(
+                            color: color, 
+                            
+                            decoration: TextDecoration.none, 
+                        ),
+                    ),
+                    trailing: Icon(
+                        icon, 
+                        color: color,
+                    ),
+                );
+              },
+            );
+          } else {
+            return const Center(child: Text('Tidak ada data Todo.'));
+          }
+        },
+      ),
+    );
   }
 }
